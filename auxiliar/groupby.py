ebe790346f89c1379dbd5ce70d55c26b3d06a200
Devthalesant <tha.santoro2000@gmail.com>
2025-01-14 23:06:56 +0000

    Updating files

--- auxiliar/groupby.py
+++ auxiliar/groupby.py
@@ -8,7 +8,7 @@ import plotly.express as px
 def grafico_barras_vendas(new_df,year_optinos):
 
   base = new_df.loc[new_df["ANO"] == year_optinos]
-  sell_groupby = new_df.groupby(["PERIODO"]).agg({"TOTAL LÍQUIDO ITEM" : "sum"}).reset_index()
+  sell_groupby = base.groupby(["PERIODO"]).agg({"TOTAL LÍQUIDO ITEM" : "sum"}).reset_index()
   sell_grafic = px.bar(sell_groupby, x="PERIODO" , y="TOTAL LÍQUIDO ITEM",title = f"Gráfico de Vendas 2025")
 
   return sell_grafic
